51dad6de7452d0527068ef97c2d471b99ddf01c9
Jakub Heppner <jakub.heppner@itsorce.com>
2026-06-03 11:40:45 +0200

    Initial Spring Boot project

---
+++ src/main/java/pl/jakubheppner/githubrepositoriesproxy/GithubRepositoriesProxyApplication.java
@@ -0,0 +1,13 @@
+package pl.jakubheppner.githubrepositoriesproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepositoriesProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRepositoriesProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/jakubheppner/githubrepositoriesproxy/GithubRepositoriesProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jakubheppner.githubrepositoriesproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepositoriesProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
